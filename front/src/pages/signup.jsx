1639538588f41ceb4f08c83207a00573a197a437
Sernny1369 <135222155+Sernny1369@users.noreply.github.com>
2025-06-27 13:13:52 +0200

    Update signup.jsx

--- front/src/pages/signup.jsx
+++ front/src/pages/signup.jsx
@@ -6,30 +6,6 @@ import "../styles/signup.css";
 
 export default function Signup() {
   const [formData, setFormData] = useState({
-<<<<<<< HEAD
-    photo: null,
-    nom: "",
-    prenom: "",
-    age: "",
-    sexe: "",
-    lieu: "",
-    bio: "",
-    contact: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value, files } = e.target;
-    setFormData({
-      ...formData,
-      [name]: files ? files[0] : value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData); // à remplacer par un appel API ou logique métier
-  };
-=======
    email: "",
    password: "",
    role: ""
@@ -67,25 +43,12 @@ export default function Signup() {
     }
   };
  
->>>>>>> 55a1c6c (Initial commit)
     return (
       <main className="main-content">
         <h1 className="title">Signup Page</h1>
         <p className="subtitle">Inscris-toi pour trouver ta future colocation !</p>
 
         <form className="signup-form" onSubmit={handleSubmit}>
-<<<<<<< HEAD
-          <label>
-            Photo:
-            <input type="file" name="photo" accept="image/*" onChange={handleChange} />
-          </label>
-
-          <input
-            type="text"
-            name="nom"
-            placeholder="Nom"
-            value={formData.nom}
-=======
           
 
           <input
@@ -93,59 +56,10 @@ export default function Signup() {
             name="email"
             placeholder="email"
             value={formData.email}
->>>>>>> 55a1c6c (Initial commit)
             onChange={handleChange}
           />
 
           <input
-<<<<<<< HEAD
-            type="text"
-            name="prenom"
-            placeholder="Prénom"
-            value={formData.prenom}
-            onChange={handleChange}
-          />
-
-          <input
-            type="number"
-            name="age"
-            placeholder="Âge"
-            value={formData.age}
-            onChange={handleChange}
-          />
-
-          <select name="sexe" value={formData.sexe} onChange={handleChange}>
-            <option value="">Sexe</option>
-            <option value="Homme">Homme</option>
-            <option value="Femme">Femme</option>
-            <option value="Autre">Autre</option>
-          </select>
-
-          <input
-            type="text"
-            name="lieu"
-            placeholder="Lieu de résidence"
-            value={formData.lieu}
-            onChange={handleChange}
-          />
-
-          <textarea
-            name="bio"
-            placeholder="Quelques mots sur toi..."
-            value={formData.bio}
-            onChange={handleChange}
-            rows="4"
-          />
-
-          <input
-            type="text"
-            name="contact"
-            placeholder="Comment te contacter ? (email, tel, etc.)"
-            value={formData.contact}
-            onChange={handleChange}
-          />
-
-=======
             type="password"
             name="password"
             placeholder="password"
@@ -159,13 +73,8 @@ export default function Signup() {
             <option value="Host">Hote</option>
           </select>
 
->>>>>>> 55a1c6c (Initial commit)
           <button type="submit">Créer un compte</button>
         </form>
       </main>
     )
-<<<<<<< HEAD
-}
-=======
 };
->>>>>>> 55a1c6c (Initial commit)
